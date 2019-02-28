11786093a3b3b2cc6ff352bea4128249598dbec6
ChrisNg <ChrNg45@gmail.com>
2019-02-27 19:28:01 -0800

    fleshed out logic.js and index.html

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,20 +1,20 @@
-function buildMetadata(sample) {
+function buildMetadata(channel) {
 
     // @TODO: Complete the following function that builds the metadata panel
-    var meta = `/metadata/${sample}`;
+    var meta = `/metadata/${channel}`;
     // Use `d3.json` to fetch the metadata for a sample
-    d3.json(meta).then(function (sample) {
-        var sample_metadata = d3.select("#sample-metadata");
+    d3.json(meta).then(function (channel) {
+        var channel_metadata = d3.select("#channel-metadata");
 
         // Use d3 to select the panel with id of `#sample-metadata`
 
         // Use `.html("") to clear any existing metadata
         sample_metadata.html("");
-        console.log(sample);
+        console.log(channel);
         // Use `Object.entries` to add each key and value pair to the panel
         // Hint: Inside the loop, you will need to use d3 to append new
         // tags for each key-value in the metadata.
-        Object.entries(sample).forEach(function ([key, value]) {
+        Object.entries(channel).forEach(function ([key, value]) {
             var row = sample_metadata.append("p");
             row.text(`${key}: ${value}`);
         });
@@ -22,3 +22,33 @@ function buildMetadata(sample) {
         // buildGauge(data.WFREQ);
     });
 }
+
+function init() {
+    // Grab a reference to the dropdown select element
+    var selector = d3.select("#selDataset");
+  
+    // Use the list of sample names to populate the select options
+    d3.json("/name").then((channelName) => {
+      sampleNames.forEach((channel) => {
+        input
+          .append("option")
+          .text(channelName)
+          .property("value", sample);
+      });
+  
+      // Use the first sample from the list to build the initial plots
+      const firstSample = sampleNames[0];
+      buildCharts(firstChannel);
+      buildMetadata(firstChannel);
+    });
+  }
+  
+  function optionChanged(newChannel) {
+    // Fetch new data each time a new sample is selected
+    // buildCharts(newChannel);
+    buildMetadata(newChannel);
+  }
+  
+  // Initialize the dashboard
+  init();
+  
