9b1f29fdc1e3c994077f5d6987d6bc29391426d1
paulina <paulinatharail@yahoo.com>
2019-02-27 19:37:42 -0800

    PP app.py

---
+++ app.py
@@ -0,0 +1,51 @@
+import os
+
+import pandas as pd
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+
+from flask import Flask, jsonify, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+#################################################
+# Database Setup
+#################################################
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///YouTube_Top_Channels.sqlite"
+db = SQLAlchemy(app)
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(db.engine, reflect=True)
+
+# Save references to each table
+#Samples_Metadata = Base.classes.sample_metadata
+#Samples = Base.classes.samples
+
+@app.route("/")
+def index():
+    """Return the homepage."""
+    return render_template("index.html")
+
+
+@app.route("/data")
+def JSON_data():
+    """Return JSONified data."""
+
+    # Use Pandas to perform the sql query
+    stmt = db.session.query(Channel).statement
+    df = pd.read_sql_query(stmt, db.session.bind)
+
+    return jsonify(df)
+
+if __name__ == "__main__":
+    app.run()
+
+
